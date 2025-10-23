f6c1e4cf131d4c6adc0e178c956ef23848fba4a9
Rodrigo Darío García Aguiñaga <l23590230@sjuanrio.tecnm.mx>
2025-10-22 22:04:17 -0600

    Update Invertir_Cadena.py

--- Unidad 2/Invertir_Cadena.py
+++ Unidad 2/Invertir_Cadena.py
@@ -1,1 +1,8 @@
+def invertir_cadena(cadena):
+    if len(cadena) == 0:
+        return cadena
+    else:
+        return cadena[-1] + invertir_cadena(cadena[:-1])
+
+print(invertir_cadena("hola"))  # "aloh"
 
