cc3a48f7a948e9853419053fa86d500cea75018b
Rodrigo Darío García Aguiñaga <l23590230@sjuanrio.tecnm.mx>
2025-10-23 20:50:34 -0600

    Update Invertir_Entero.py

--- Unidad 2/Invertir_Entero.py
+++ Unidad 2/Invertir_Entero.py
@@ -1,1 +1,11 @@
+def invertirEntero(n):
+    if type(n) != int or n < 1:
+        raise Exception("n debe ser entero positivo.")
+    return invertirEnteroAux(n)
 
+def invertirEnteroAux(n):
+    if n < 10:
+        return n
+    else:
+        return (n % 10) * 10**contarDigitos(n // 10) \
+             + invertirEnteroAux(n // 10)
