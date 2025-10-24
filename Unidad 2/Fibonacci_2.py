6ed06db041c26494a762bb28ef450c87da6dd45c
Rodrigo Darío García Aguiñaga <l23590230@sjuanrio.tecnm.mx>
2025-10-23 21:52:21 -0600

    Update Fibonacci_2.py

--- Unidad 2/Fibonacci_2.py
+++ Unidad 2/Fibonacci_2.py
@@ -1,1 +1,22 @@
+def fibonacci(n):
+    # Caso base: El valor es 0 o 1
+    if n == 0 or n == 1:
+        # Mostramos el valor base devuelto para ayudar a rastrear.
+        # Esto ocurre muchas veces.
+        # print(f"Caso base alcanzado: fibonacci({n}) devuelve {n}")
+        return n
+    else:
+        # Llamadas recursivas para obtener los dos términos anteriores
+        a = fibonacci(n - 1)
+        b = fibonacci(n - 2)
+        
+        # Realizamos la suma
+        resultado = a + b
+        
+        # Mostramos la operación de suma:
+        print(f"Calculando fibonacci({n}): {a} + {b} = {resultado}")
+        
+        return resultado
 
+print(f"\nCalculando fibonacci(6):\n")
+print(f"El resultado final es: {fibonacci(6)}\n")
