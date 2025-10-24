0d816c7bd418f7a3cdb0d212557b63721f163c8b
Rodrigo Darío García Aguiñaga <l23590230@sjuanrio.tecnm.mx>
2025-10-23 20:43:59 -0600

    Update Contar_Digitos.py

--- Unidad 2/Contar_Digitos.py
+++ Unidad 2/Contar_Digitos.py
@@ -1,1 +1,10 @@
+def contarDigitos(n):
+    if type(n) != int or n < 0:
+        raise Exception("n debe ser entero no negativo.")
+    return contarDigitosAux(n)
 
+def contarDigitosAux(n):
+    if n < 10:
+        return 1
+    else:
+        return contarDigitosAux(n // 10) + 1
