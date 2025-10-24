a5b9141d318a5957d03b1b0dd4eedd2ba97c4b8b
Rodrigo Darío García Aguiñaga <l23590230@sjuanrio.tecnm.mx>
2025-10-23 20:35:32 -0600

    Update Imprimir_Pares_M_Y_N.py

--- Unidad 2/Imprimir_Pares_M_Y_N.py
+++ Unidad 2/Imprimir_Pares_M_Y_N.py
@@ -1,1 +1,16 @@
+def imprimirImparesEntreMyN(m, n):
+    if type(m) != int:
+        raise Exception("m debe ser entero positivo.")
+    if type(n) != int or n <= m:
+        raise Exception("n debe ser entero mayor que m.")
 
+    m = m + 1 if m % 2 == 0 else m
+    n = n - 1 if n % 2 == 0 else n
+    imprimirImparesEntreMyNAux(m, n)
+
+def imprimirImparesEntreMyNAux(m, n):
+    if m > n:
+        print()
+    else:
+        print(m, end = " ")
+        imprimirImparesEntreMyNAux(m + 2, n)
