d50b1eeb87e0837def64471070b6d158d47a98d0
Rodrigo Darío García Aguiñaga <l23590230@sjuanrio.tecnm.mx>
2025-10-23 20:46:19 -0600

    Update Sumar_Digitos.py}

--- Unidad 2/Sumar_Digitos.py
+++ Unidad 2/Sumar_Digitos.py
@@ -1,1 +1,10 @@
+def sumarDigitos(n):
+    if type(n) != int or n < 0:
+        raise Exception("n debe ser entero no negativo.")
+    return sumarDigitosAux(n)
 
+def sumarDigitosAux(n):
+    if n < 10:
+        return n
+    else:
+        return sumarDigitosAux(n // 10) + n % 10
