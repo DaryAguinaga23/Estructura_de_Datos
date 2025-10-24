b425558db52f4506db996c55c1f2ce9fb38aa5aa
Rodrigo Darío García Aguiñaga <l23590230@sjuanrio.tecnm.mx>
2025-10-23 20:31:12 -0600

    Update Imprimir_Pares_Hasta_N.py

--- Unidad 2/Imprimir_Pares_Hasta_N.py
+++ Unidad 2/Imprimir_Pares_Hasta_N.py
@@ -1,1 +1,13 @@
+def imprimirParesHastaN(n):
+    if type(n) != int or n < 1:
+        raise Exception("n debe ser entero positivo.")
+    n -= n % 2
+    imprimirParesHastaNAux(n)
+    print()
 
+def imprimirParesHastaNAux(n):
+    if n == 0:
+        return
+    else:
+        imprimirParesHastaNAux(n - 2)
+        print(n, end = " ")
